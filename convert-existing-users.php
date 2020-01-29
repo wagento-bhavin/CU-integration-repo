5f3ea73addc91402f0266955dcfcecdefb55b8fe
Bhavin Parmar <bhavin@wagento.com>
2020-01-29 10:50:51 +0530

    updated code for migrate all customer and added new file for creating category structure(CU-11)

--- convert-existing-users.php
+++ convert-existing-users.php
@@ -2,14 +2,13 @@
 
 //DB Credentials
 $servername = "localhost";
-/*$username = "cemmlxmy_admin";
+$username = "cemmlxmy_admin";
 $password = 'tKR+uEee?7RS';
-$dbname = "cemmlxmy_users";*/
-$username = "ycfssmjzrs";
+$dbname = "cemmlxmy_users";
+/*$username = "ycfssmjzrs";
 $password = 'BPc98qqeVA';
-$dbname = "ycfssmjzrs";
+$dbname = "ycfssmjzrs";*/
 
-//$api_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkNsaWVudCIsInN1YiI6ImUyZGE2ODE4LWYxMWUtNDNhNS1iOGUxLTQyZjRjNDU5ZWI1MyJ9.eyJqdGkiOiJkNTM4NjI4NS05NDliLTQ5NTEtYjg2Ni1mNmUzNGEwY2ZhOTIiLCJleHAiOjQ3MTYwNDA5OTksImRhdGEiOnsiY2xpZW50X2lkIjoiZTJkYTY4MTgtZjExZS00M2E1LWI4ZTEtNDJmNGM0NTllYjUzIiwicGF5bG9hZCI6eyJjbGllbnQiOnsidXVpZCI6ImUyZGE2ODE4LWYxMWUtNDNhNS1iOGUxLTQyZjRjNDU5ZWI1MyJ9fX19.2Q1_PybkK684egs6stcVGmxkdetDAhSpbWFXFBEE0KY";
 $api_key = "eyJ0eXAiOiJDbGllbnQiLCJzdWIiOiJlMmRhNjgxOC1mMTFlLTQzYTUtYjhlMS00MmY0YzQ1OWViNTMiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIwMGVlODg0My1mOWI2LTRmZjMtYmFjMC1jYzVkZWJjZTA3NTciLCJleHAiOjE4OTQ5MTMxNTQsImRhdGEiOnsiY2xpZW50X2lkIjoiZTJkYTY4MTgtZjExZS00M2E1LWI4ZTEtNDJmNGM0NTllYjUzIiwicGF5bG9hZCI6eyJjbGllbnQiOnsidXVpZCI6ImUyZGE2ODE4LWYxMWUtNDNhNS1iOGUxLTQyZjRjNDU5ZWI1MyJ9fX19.QA77p82AicXUxpIt-TOvEtopx4ikExctv8RvyDe8iPc";
 $partner_uuid 	= '6ea10786-ea8f-46cf-bbdd-02f9ff9a87e5';
 $authorization = 'Authorization: Bearer ' . $api_key;
@@ -26,7 +25,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$count_url = "https://4d87206512af68fc951ce70348bb8105:10ee2c6ba02eef12fdda08912d318c49@pixielanekidz.myshopify.com/admin/api/2020-01/customers/count.json";
+$count_url = "https://4d87206512af68fc951ce70348bb8105:10ee2c6ba02eef12fdda08912d318c49@pixielanekidz.myshopify.com/admin/api/2019-04/customers/count.json";
 $total = file_get_contents($count_url);
 $total = json_decode($total,true);
 $total = $total['count'];
@@ -34,7 +33,7 @@ $page = ceil($total / 250);
 $count = 0;
 $wholesale_count = 0;
 for ($i=1; $i <= $page ; $i++) {
-    $url = "https://4d87206512af68fc951ce70348bb8105:10ee2c6ba02eef12fdda08912d318c49@pixielanekidz.myshopify.com/admin/api/2020-01/customers.json?limit=250&page=".$i;
+    $url = "https://4d87206512af68fc951ce70348bb8105:10ee2c6ba02eef12fdda08912d318c49@pixielanekidz.myshopify.com/admin/api/2019-04/customers.json?limit=250&page=".$i;
     $contents = file_get_contents($url);
     $customers = json_decode($contents,true);
     $customers = $customers["customers"];
@@ -103,8 +102,8 @@ for ($i=1; $i <= $page ; $i++) {
         $updated= $result->updated_at;
         $tags 	= implode(';', $tags);
 
-        $sql = "INSERT INTO cheddarup_users (shopify_id, email, cheddarup_id, uuid, created_at, updated_at, tags, token)
-	VALUES ('{$customer_id}','{$email}','{$id}','{$uuid}','{$created}','{$updated}', '{$tags}', '{$token}')";
+        $sql = "INSERT INTO cheddarup_users (shopify_id, email, cheddarup_id, uuid, created_at, updated_at, token)
+	VALUES ('{$customer_id}','{$email}','{$id}','{$uuid}','{$created}','{$updated}', '{$token}')";
         if ($conn->query($sql) === TRUE) {
             echo "New record created successfully";
         } else {
